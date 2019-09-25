58b864e7be8c704b99c1760155602e41fa17c082
Raghad Abdulghani <raghad.at@gmail.com>
2019-09-25 13:56:17 +0300

    Update person.model.ts

--- packages/person-cc/src/person.model.ts
+++ packages/person-cc/src/person.model.ts
@@ -7,6 +7,31 @@ import {
   Validate
 } from '@worldsibu/convector-core-model';
 
+export class Attribute extends ConvectorModel<Attribute>{
+  @ReadOnly()
+  @Required()
+  public readonly type = "io.worldsibu.attribute"
+
+  @Required()
+  public content: any;
+
+  @Required()
+  @ReadOnly()
+  @Validate(yup.number())
+  public issuedDate: Number;
+
+  public expiresDate: Date;
+
+  @Default(false)
+  @Validate(yup.boolean())
+  public expired: boolean;
+
+  @Required()
+  @Validate(yup.string())
+  public certifierID: string;
+
+}
+
 export class Person extends ConvectorModel<Person> {
   @ReadOnly()
   @Required()
@@ -16,12 +41,15 @@ export class Person extends ConvectorModel<Person> {
   @Validate(yup.string())
   public name: string;
 
-  @ReadOnly()
-  @Required()
-  @Validate(yup.number())
-  public created: number;
+  @Validate(yup.array(Attribute.schema()))
+  public attributes: Array<Attribute>;
 
-  @Required()
-  @Validate(yup.number())
-  public modified: number;
+  // @ReadOnly()
+  // @Required()
+  // @Validate(yup.number())
+  // public created: number;
+
+  // @Required()
+  // @Validate(yup.number())
+  // public modified: number;
 }
