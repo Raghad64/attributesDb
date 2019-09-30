f587cd2354f5cc4f4d148f2dfa5f90b0311afa1d
Raghad Mah <raghad@Raghads-MacBook-Air.local>
2019-09-30 15:21:56 +0300

    Interface Change

---
+++ Index/login.js
@@ -0,0 +1,28 @@
+// function validate(name, password){
+//     var sadaraUsername = document.getElementById("name-form1-f").value;
+//  var sPassword = document.getElementById("password-form1-f").value;
+    
+//     //alert (id);
+//     window.location = "SadaraPage.html";
+    
+
+    // Redirecting to other page.
+    //return false;
+    //}
+// }
+
+
+function validate(id){
+//var sadaraUsername = document.getElementById("name-form1-f").value;
+// var sPassword = document.getElementById("password-form1-f").value;
+    
+if(id == 1){    window.location.href = "SadaraPage.html";}
+else if (id == 2){
+    window.location.href = "CustomsPage.html"
+}
+    
+
+    // Redirecting to other page.
+    //return false;
+    //}
+}
