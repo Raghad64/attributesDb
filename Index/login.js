2eb304c4ea0ca0cebd2a60a89e0f36af04a411df
Raghad Mah <raghad@Raghads-MacBook-Air.local>
2019-09-30 21:00:41 +0300

    loginCompleted

--- Index/login.js
+++ Index/login.js
@@ -13,16 +13,8 @@
 
 
 function validate(id){
-//var sadaraUsername = document.getElementById("name-form1-f").value;
-// var sPassword = document.getElementById("password-form1-f").value;
-    
 if(id == 1){    window.location.href = "SadaraPage.html";}
 else if (id == 2){
     window.location.href = "CustomsPage.html"
 }
-    
-
-    // Redirecting to other page.
-    //return false;
-    //}
 }
